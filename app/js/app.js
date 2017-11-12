225f24c358399195d4a91a7f8cabec678fbab397
Lara <lostintheuniverse50@gmail.com>
2017-11-12 21:44:13 +0100

    app initial setup

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,23 @@
-console.log('sdfsfsdfsd');
+console.log('sdfsjfsdfsd');
+
+var baraja = [];
+
+var croupier = {};
+
+croupier.barajar = function () {};
+
+var jugador = {};
+
+jugador.mano = [];
+
+jugador.pasar = function (){
+    if (this.mano[0] > 9) {
+        return true;
+    }
+};
+
+function blackjack () {
+    var miBaraja =  baraja;
+    var miCroupier = croupier;
+    var miJugador = jugador;
+}
